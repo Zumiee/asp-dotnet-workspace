9487ebcc9e8689ceaa8f276a5e5309759476b97e
Jaycee <jccantos1212@gmail.com>
2024-10-17 21:34:53 +0800

    feat: add games endpoint

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,37 @@
+using WebApplication1.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello Worlds!");
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter VI",
+        "Fighting",
+        20.99M,
+        new DateOnly(2020, 7, 15)
+    ),
+    new (
+        2,
+        "Valorant",
+        "FPS",
+        20.99M,
+        new DateOnly(2021, 7, 15)
+    ),
+    new (
+        3,
+        "League of Legends",
+        "MOBA",
+        20.99M,
+        new DateOnly(2022, 7, 15)
+    ),
+
+];
+
+// Get games/
+app.MapGet("games", () => games);
+
+// Get games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
 
 app.Run();
