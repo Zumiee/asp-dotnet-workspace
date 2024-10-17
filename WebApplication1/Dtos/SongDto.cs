c74402b97d706d5d28615d2f718ffc906005b01d
Jaycee <jccantos1212@gmail.com>
2024-10-17 21:54:51 +0800

    feat: remove game track DTO

--- WebApplication1/Dtos/SongDto.cs
+++
@@ -1,3 +0,0 @@
-namespace WebApplication1.Dtos;
-
-public record class SongDto(int Id, string Name, string Genre, DateOnly ReleaseDate);
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9487ebcc9e8689ceaa8f276a5e5309759476b97e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4430a5ad4d287c6ec1fcce617a24f811874ae491")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
