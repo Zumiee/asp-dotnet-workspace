0974c3b6635f8a12034853b5d4bc601b7c186576
Jaycee <jccantos1212@gmail.com>
2024-10-21 10:31:47 +0800

    feat: add create game endpoint and DTO

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -3,6 +3,8 @@ using WebApplication1.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+const string GetGameEndpointName = "GetGame";
+
 List<GameDto> games = [
     new (
         1,
@@ -22,11 +24,24 @@ List<GameDto> games = [
 ];
 
 
-// Get games/
+// GET /games/
 app.MapGet("games", () => games);
 
-// Get games/1
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id));
+// GET /games/1
+app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName(GetGameEndpointName);
 
+// POST /games
+app.MapPost("games", (CreateGameDto newGame) => {
+    GameDto game = new(
+        games.Count + 1,
+        newGame.Name,
+        newGame.Genre,
+        newGame.Price,
+        newGame.ReleaseDate
+    );
+    games.Add(game);
+
+    return Results.CreatedAtRoute(GetGameEndpointName, new {id = game.Id}, game);
+});
 
 app.Run();
--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa88fa948263f87e40d44f8288cde214c8f96b01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd8e8187d5c7b48c29ba3992753bb4d8a717b2c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
