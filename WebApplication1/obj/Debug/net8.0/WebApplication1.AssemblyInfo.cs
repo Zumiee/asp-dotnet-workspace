caaae2b4811de6990c8e6f96a5ad7cd1158039b1
Jaycee <jccantos1212@gmail.com>
2024-10-22 20:24:57 +0800

    build: implement Entity Framework

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61aa2f7e5e29670b99648e910994020fce15feb3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8c8dedd8f1bd954efeb1fd06c45f515cdaef105")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
