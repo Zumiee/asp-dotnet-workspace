3ec67440e7045b2f60ce1663ed0a0fb84eb079c6
Jaycee <jccantos1212@gmail.com>
2024-10-23 16:16:24 +0800

    fix: remove unncessary usings in Genre entity

--- WebApplication1/Entities/Genre.cs
+++ WebApplication1/Entities/Genre.cs
@@ -1,4 +1,4 @@
-using System;
+
 
 namespace WebApplication1.Entities;
 
@@ -6,5 +6,5 @@ public class Genre
 {
     public int Id {get; set;}
 
-    public string Name {get; set;}
+    public required string Name {get; set;}
 }
