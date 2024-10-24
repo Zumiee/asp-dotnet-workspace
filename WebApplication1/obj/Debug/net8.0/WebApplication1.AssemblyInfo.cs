ba2f48e6a246c2a27e95aa7b962a0946853c208c
Jaycee <jccantos1212@gmail.com>
2024-10-24 16:58:34 +0800

    feat: add extension class to generate db on start up

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a5c1b36d3f548dde533dc0ace86f50429f5d34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9af9bd9318513e35120cd3d1eddda2c05113d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
