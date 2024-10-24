2675e7e2c3faf3fc4c0909bc9b26f8971ccd69d2
Jaycee <jccantos1212@gmail.com>
2024-10-24 17:10:06 +0800

    build: migrate ef and populate db with samples

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba2f48e6a246c2a27e95aa7b962a0946853c208c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26fa238f14dc74cd2883a3c2b929154dd48cab35")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
